2d8a70f6be165b95ee7b4755dd89166bc5f997f5
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2024-10-03 23:26:19 +0600

    Section active button added

--- scripts/video.js
+++ scripts/video.js
@@ -1,18 +1,24 @@
-function getTime(time){
-    const hour = parseInt(time /3600);
-    let remainingSecond = time % 3600;
-    let minute = parseInt(remainingSecond / 60);
-    remainingSecond =parseInt(remainingSecond%60)
-    return `${hour} hour ${minute} minute ago`
-    
+function getTime(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  let minute = parseInt(remainingSecond / 60);
+  remainingSecond = parseInt(remainingSecond % 60);
+  return `${hour} hour ${minute} minute ago`;
 }
-console.log(getTime(4326));
-
+// console.log(getTime(4326));
 
 /**
  * 1.Fetch ,Load and Show Categories
  */
 
+const removeActiveClass = () =>{
+    const buttons = document.getElementsByClassName("category-btn");
+    console.log(buttons);
+    for (const btn of buttons) {
+        btn.classList.remove("active")
+    }
+    
+}
 // Create Load Catagories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -29,32 +35,43 @@ const loadVideos = () => {
     .catch((error) => console.log(error));
 };
 
-// const detaiels ={
-//     {
-//         "category_id": "1001",
-//         "video_id": "aaad",
-//         "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
-//         "title": "Smells Like Teen Spirit",
-//         "authors": [
-//             {
-//                 "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
-//                 "profile_name": "Oliver Harris",
-//                 "verified": true
-//             }
-//         ],
-//         "others": {
-//             "views": "5.4K",
-//             "posted_date": "1672656000"
-//         },
-//         "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebell"
-//     }
-// }
+const loadCategoryVideos = (id) => {
+  //   alert(id);
+  //fetch
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
 
+        //remove active color class 
+        removeActiveClass();
+
+        const activeBtn = document.getElementById(`btn-${id}`);
+        activeBtn.classList.add("active");
+        displayVideos(data.category)
+       
+        
+    })
+    .catch((error) => console.log(error));
+};
 //DIsplay Videos
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+    <div class ="min-h-[300px] flex flex-col gap-5 justify-center items-center">
+    <h2 class ="text-center text-xl font-bold">NO CONTENT HERE IN THIS CATAGORY</h2>
+    <img src ="assets/Icon.png"/>
+    </div>
+    `;
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
   videos.forEach((video) => {
-    console.log(video);
+    // console.log(video);
 
     const card = document.createElement("div");
     card.classList = "card card-compact";
@@ -63,18 +80,30 @@ const displayVideos = (videos) => {
     <img
       src=${video.thumbnail} 
       class = "h-full w-full object-cover"/>
-      ${video.others.posted_date?.length == 0? "" : `<span class="absolute right-2 bottom-2 text-white bg-black rounded p-1">${getTime(video.others.posted_date)}</span>`}
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 text-white text-xs bg-black rounded p-1">${getTime(
+              video.others.posted_date
+            )}</span>`
+      }
       
   </figure>
   <div class="px-0 py-2 flex gap-2">
    <div>
-   <img class="w-10 h-10 rounded-full object-cover " src=${video.authors[0].profile_picture} />
+   <img class="w-10 h-10 rounded-full object-cover " src=${
+     video.authors[0].profile_picture
+   } />
    </div>
    <div>
   <h2 class="font-bold">${video.title}</h2>
   <div class="flex items-center">
   <p >${video.authors[0].profile_name}</p>
- ${video.authors[0].verified == true ? ` <img class ="w-5 h-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>` : ""}
+ ${
+   video.authors[0].verified == true
+     ? ` <img class ="w-5 h-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>`
+     : ""
+ }
   </div>
   <div class='flex'>
   <p>${video.others.views} </p>
@@ -92,12 +121,17 @@ const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categories");
   categories.forEach((item) => {
     // Create button
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
+    const buttonContainer = document.createElement("div");
+    // console.log(item);
+
+    buttonContainer.innerHTML = `
+    <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
+    ${item.category}
+    </button>
+    `;
 
     //add button to category container
-    categoryContainer.append(button);
+    categoryContainer.append(buttonContainer);
   });
 };
 
